0d1bd87a874ca8d0f2e55c757e9a55a011a19cc7
Muhammad Waseem Akhtar <waseemakhtar@pk.folio3.com>
2025-06-17 14:24:18 +0500

    Fixes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const querystring = require('querystring');
 const app = express();
 
 // Load environment variables (manually or use dotenv for production)
-const CLIENT_ID = '90681a4f-5ea6-48c5-b54a-a46e5e16b537';
-const CLIENT_SECRET = '01e5006d-16c2-4947-9aea-e8ec04f5b5e4';
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 app.use(express.json());
 
@@ -18,8 +18,8 @@ app.post('/adp/token', (req, res) => {
 
     const postData = querystring.stringify({
         grant_type: 'client_credentials',
-        client_id: '90681a4f-5ea6-48c5-b54a-a46e5e16b537',
-        client_secret: '01e5006d-16c2-4947-9aea-e8ec04f5b5e4'
+        client_id: process.env.CLIENT_ID || CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET || CLIENT_SECRET,
     });
 
     const options = {
